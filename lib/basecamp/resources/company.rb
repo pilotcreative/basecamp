f08230b9e78fdfc66da9a6912102c408ef4af586
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2011-11-25 14:32:44 +0100

    added people to company

--- lib/basecamp/resources/company.rb
+++ lib/basecamp/resources/company.rb
@@ -1,4 +1,9 @@
 module Basecamp
   class Company < Resource
+    def people
+      @people = Proxy.new(Person, self.connection_attributes)
+      @people.prefix = "/companies/#{self.id}/"
+      @people
+    end
   end
 end
