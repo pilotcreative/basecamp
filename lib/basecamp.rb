af031d51078318caab00fc27249be9aebe086baa
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2011-12-15 14:31:14 +0100

    throttle connection

--- lib/basecamp.rb
+++ lib/basecamp.rb
@@ -13,3 +13,23 @@ require "basecamp/resources/comment"
 require "basecamp/resources/message"
 
 require "basecamp/connection"
+
+
+class ActiveResource::Connection
+  cattr_accessor :requests
+  def http
+    @@requests ||= 0
+    throttle if @@requests >= 500
+    http = Net::HTTP.new(@site.host, @site.port)
+    http.use_ssl = @site.is_a?(URI::HTTPS)
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE if http.use_ssl
+    http.read_timeout = @timeout if @timeout
+    @@requests += 1
+    http
+  end
+private
+  def throttle
+    sleep 10
+    @@requests = 0
+  end
+end
